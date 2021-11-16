aa6cc006200544e579da928110c325a4b478756c
PlamenMichev <plamenmichev15@gmail.com>
2021-11-16 22:53:51 +0100

    make navigation for mobile

---
+++ src/common.js
@@ -0,0 +1,12 @@
+function toggleHamburger(element) {
+    const mobileNavElements = document.getElementsByClassName('nav-links-mobile')[0];
+    const isHidden = mobileNavElements.classList.contains('nav-links-mobile-hidden')
+
+    if (isHidden) {
+        mobileNavElements.classList.remove('nav-links-mobile-hidden');
+    } else {
+        mobileNavElements.classList.add('nav-links-mobile-hidden');
+    }
+
+    element.classList.toggle("change");
+}
